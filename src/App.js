1f71d8519d3394c12dccf995fdf4558f7752af52
Vinicius Gomes <viniciusemog@hotmail.com>
2022-03-07 15:18:18 -0300

    Resoluçao do requisito 1 - Criando rotas para aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
+
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Login } exact />
+
+          <Route path="/search" component={ Search } />
+
+          <Route path="/album/:id" component={ Album } />
+
+          <Route path="/favorites" component={ Favorites } />
+
+          <Route path="/profile" component={ Profile } exact />
+
+          <Route path="/profile/edit" component={ ProfileEdit } exact />
+
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
