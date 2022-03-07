868755e0febe2a2939ea300aa2f69ee06f2c2bed
Vinicius Gomes <viniciusemog@hotmail.com>
2022-03-07 14:24:45 -0300

    Initial commit project Trybetunes

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -2,6 +2,7 @@ const getMusics = async (id) => {
   const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
   const requestJson = await request.json();
   return requestJson.results;
+  
 };
 
 export default getMusics;
