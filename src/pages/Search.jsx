306e2838993edf82de8ef5f823a861d483beb346
Vinicius Gomes <viniciusemog@hotmail.com>
2022-03-08 03:39:30 -0300

    Resoluçao do requisito 5 - Criando formulario de pesquisa

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../components/Header';
+import './Search.css';
 
 class Search extends React.Component {
   constructor() {
@@ -8,7 +9,11 @@ class Search extends React.Component {
 
     this.state = {
       searchClass: '',
+      name: '',
+      entryButton: false,
     };
+
+    this.handleInput = this.handleInput.bind(this);
   }
 
   componentDidMount() {
@@ -19,12 +24,56 @@ class Search extends React.Component {
     });
   }
 
+  handleInput({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => this.handleEntryButton());
+  }
+
+  handleEntryButton() {
+    const { name } = this.state;
+
+    let disabledEntryButton = false;
+    const enableButtonCondition = 2;
+
+    if (name.length >= enableButtonCondition) disabledEntryButton = true;
+
+    this.setState({
+      entryButton: disabledEntryButton,
+    });
+  }
+
   render() {
-    const { searchClass } = this.state;
+    const { searchClass, entryButton } = this.state;
 
     return (
       <div data-testid="page-search">
         <Header searchClass={ searchClass } />
+
+        <div>
+          <form className="search-artist-form-section">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+              name="name"
+              className="artist-name-input"
+              onChange={ this.handleInput }
+            />
+            <br />
+            <button
+              type="button"
+              className="search-artist-button-input"
+              data-testid="search-artist-button"
+              onClick={ this.handleClick }
+              disabled={ !entryButton }
+            >
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
